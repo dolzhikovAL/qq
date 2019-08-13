19f77c4f3586d2747c0457b16530c600154fbd11
Dolzhykov Aleksander <dolzhikov.al@yandex.ua>
2019-08-14 00:14:17 +0300

    gthtl geitv

---
+++ src/qqq1.java
@@ -0,0 +1,17 @@
+public class qqq1 {
+    public static void main(String[] args) {
+        System.out.println("print "+(1 + 1));
+
+        byte b = 20;
+        short s = 3000;
+        int i = 320000;
+        long l = 124313241313l;
+        float f = 2.33f;
+        double d = 123123.4;
+        boolean bl = true;
+        char c = '0';
+        // System.out.println(s + i);
+        String s1 = "21";
+        System.out.println("s1"+1);
+    }
+}
